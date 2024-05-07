e6de12ab1485e4302234686c932da34e483c402d
Zach Andrade <zach_andrade@yahoo.com>
2024-05-07 14:44:42 -0500

    attempting to fix relocating to login page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -118,7 +118,7 @@ function App() {
               <Redirect to="/user" />
               :
               // Otherwise, show the Landing page
-              <LandingPage />
+              <LoginPage />
             }
           </Route>
 
